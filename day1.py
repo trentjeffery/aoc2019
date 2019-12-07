685f9112d5a4255d7a4e2a7d65498f3962d66eac
Trent Jeffery <trentdjeffery@gmail.com>
2019-12-07 12:42:33 -0800

    day1 part 2

--- day1.py
+++ day1.py
@@ -19,4 +19,26 @@ def part1():
             sum += CalculateFuelToLaunch(val)
         print(sum)
 
-print(part1())
+#print(part1())
+
+def CalculateTotalFuelToLaunch(moduleMass):
+    sum = 0
+    nextFuel = CalculateFuelToLaunch(moduleMass)
+    while nextFuel > 0:
+        sum += nextFuel
+        nextFuel = CalculateFuelToLaunch(nextFuel)
+    return sum
+
+print(CalculateTotalFuelToLaunch(14))
+print(CalculateTotalFuelToLaunch(1969))
+print(CalculateTotalFuelToLaunch(100756))
+
+def part2():
+     with open("input.XSCORE.txt") as input_file:
+        sum = 0
+        for line in input_file:
+            moduleMass = int(line)
+            sum += CalculateTotalFuelToLaunch(moduleMass)
+        print(sum)
+
+part2()
